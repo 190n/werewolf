6e5e8c70e31d0bcf2c4bbe4e315030af195adfae
Ben Grant <benjamin.j.grant@gmail.com>
2020-07-04 11:34:44 -0700

    Add ToggleButton, FlexibleContainer

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
@@ -12,52 +12,69 @@ import JoinOrCreate from './JoinOrCreate';
 import Join from './Join';
 import InGameDispatch from './InGameDispatch';
 import Create from './Create';
-import { Root, Button, ButtonGroup, FormControl, Input } from './ui';
+import { Root, Button, ButtonGroup, FormControl, Input, ToggleButton, FlexibleContainer } from './ui';
 import theme from './theme';
 
-const App = ({ store }: StoreProps): JSX.Element => (
-    <Router>
-        <Switch>
-            <Route path="/:gameId/:playerId/play">
-                <InGameDispatch store={store} />
-            </Route>
-            <Route path="/create">
-                <Create />
-            </Route>
-            <Route path="/styled">
-                <ThemeProvider theme={theme}>
-                    <Root>
-                        <p>
-                            foo bar<br />
-                            baz qux<br />
-                            wom bat
-                        </p>
-                        <p>
-                            Lorem ipsum...
-                        </p>
-                        <h1>Heading</h1>
-                        <h2>Subheading!</h2>
-                        <h3>Level 3</h3>
-                        <ButtonGroup>
-                            <Button>Click me!</Button>
-                            <Button color="#008000">Green button</Button>
-                            <Button big={true}>Thicc button</Button>
-                        </ButtonGroup>
-                        <FormControl>
-                            Label:
-                            <Input type="text" placeholder="field" />
-                        </FormControl>
-                    </Root>
-                </ThemeProvider>
-            </Route>
-            <Route path="/:gameId">
-                <Join store={store} />
-            </Route>
-            <Route path="/">
-                <JoinOrCreate />
-            </Route>
-        </Switch>
-    </Router>
-);
+const App = ({ store }: StoreProps): JSX.Element => {
+    const [checked, setChecked] = useState(false);
+
+    return (
+        <Router>
+            <Switch>
+                <Route path="/:gameId/:playerId/play">
+                    <InGameDispatch store={store} />
+                </Route>
+                <Route path="/create">
+                    <Create />
+                </Route>
+                <Route path="/styled">
+                    <ThemeProvider theme={theme}>
+                        <Root>
+                            <FlexibleContainer width="500px">
+                                <p>
+                                    foo bar<br />
+                                    baz qux<br />
+                                    wom bat
+                                </p>
+                                <p>
+                                    Lorem ipsum...
+                                </p>
+                                <h1>Heading</h1>
+                                <h2>Subheading!</h2>
+                                <h3>Level 3</h3>
+                                <ButtonGroup>
+                                    <Button>Click me!</Button>
+                                    <Button color="#008000">Green button</Button>
+                                    <Button big={true}>Thicc button</Button>
+                                </ButtonGroup>
+                                <FormControl>
+                                    Enter your name:
+                                    <Input type="text" placeholder="Firstname Lastname" />
+                                </FormControl>
+                                <FormControl>
+                                    What's your favorite flavor of ice cream?
+                                    <Input type="text" placeholder="Mint chip" />
+                                </FormControl>
+                                <ToggleButton
+                                    big={false}
+                                    checked={checked}
+                                    onChange={() => setChecked(!checked)}
+                                >
+                                    Hello
+                                </ToggleButton>
+                            </FlexibleContainer>
+                        </Root>
+                    </ThemeProvider>
+                </Route>
+                <Route path="/:gameId">
+                    <Join store={store} />
+                </Route>
+                <Route path="/">
+                    <JoinOrCreate />
+                </Route>
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
--- src/frontend/ui.tsx
+++ src/frontend/ui.tsx
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import React from 'react';
+import styled, { css } from 'styled-components';
 import { transparentize, mix } from 'polished';
 
 import theme from './theme';
@@ -6,7 +7,7 @@ import theme from './theme';
 export const Root = styled.div`
     font-family: ${props => props.theme.fonts.body};
     color: ${props => props.theme.colors.fg};
-    background-image: linear-gradient(silver, silver 12.5%, white 12.5%, white);
+    // background-image: linear-gradient(#ddd, #ddd 12.5%, white 12.5%, white);
     background-size: 100% 0.5rem;
     line-height: 1.5rem;
 
@@ -45,11 +46,12 @@ export const Button = styled.button<ButtonProps>`
     cursor: pointer;
     border-radius: ${props => props.big ? '0.375rem' : '0.25rem'};
     padding: 0 ${props => props.big ? '1.5rem' : '1rem'};
-    box-shadow: 0 0.15rem 0.25rem ${props => transparentize(0.5, props.color!)};
+    box-shadow: 0 0.125rem 0.25rem ${props => transparentize(0.5, props.color!)};
     transition: 100ms box-shadow, 100ms background-color;
 
     &:focus {
-        box-shadow: 0 0 0 0.25rem ${props => transparentize(0.5, props.color!)};
+        box-shadow: 0 0 0 0.1875rem ${props => transparentize(0.5, props.color!)};
+        outline: none;
     }
 
     &:hover {
@@ -82,5 +84,78 @@ export const ButtonGroup = styled.div`
 
 export const FormControl = styled.label`
     display: block;
+    margin-bottom: 0.75rem;
 `;
-export const Input = styled.input``;
+
+export const Input = styled.input`
+    box-sizing: border-box;
+    display: block;
+    margin: 0.25rem 0 0 0;
+    background-color: ${props => props.theme.colors.bg};
+    border: 0.125rem solid ${props => props.theme.colors.primary};
+    border-radius: 0.25rem;
+    font-size: 1rem;
+    font-family: ${props => props.theme.fonts.body};
+    height: 2rem;
+    padding: 0 0.5rem;
+    transition: 100ms box-shadow;
+    width: 100%;
+
+    &:focus {
+        box-shadow: 0 0 0 0.1875rem ${props => transparentize(0.5, props.theme.colors.primary)};
+        outline: none;
+    }
+
+    ::placeholder {
+        opacity: 0.5;
+        color: ${props => props.theme.colors.fg}
+    }
+`;
+
+export const VisuallyHidden = styled.span`
+    &:not(:focus) {
+        position: absolute;
+        height: 1px;
+        width: 1px;
+        overflow: hidden;
+        clip: rect(1px, 1px, 1px, 1px);
+        white-space: nowrap;
+    }
+`;
+
+export type ToggleButtonProps = ButtonProps & { checked?: boolean, onChange?: (e: React.FormEvent) => void };
+
+const ToggleButtonBase = styled(Button)<ToggleButtonProps>`
+    ${props => !props.checked && css<ButtonProps>`
+        background-color: transparent;
+        color: ${props => props.color};
+        border: 0.125rem solid ${props => props.color};
+        line-height: ${props => props.big ? '2.75rem' : '1.75rem'};
+        padding: 0 ${props => props.big ? '1.375rem' : '0.875rem'};
+
+        &:hover {
+            background-color: ${props => transparentize(0.75, props.color!)};
+        }
+    `}
+`;
+
+export const ToggleButton: React.FC<ToggleButtonProps> = props => (
+    <label onClick={props.onChange}>
+        <VisuallyHidden
+            as="input"
+            type="checkbox"
+            checked={props.checked}
+        />
+        <ToggleButtonBase {...props} />
+    </label>
+);
+
+export interface FlexibleContainerProps {
+    width: string;
+}
+
+export const FlexibleContainer = styled.div<FlexibleContainerProps>`
+    width: 100%;
+    max-width: ${props => props.width};
+    margin: auto;
+`;
