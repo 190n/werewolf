fab12e41aa9ae0581b71aa8dc447e4fbfe8f6296
Ben Grant <benjamin.j.grant@gmail.com>
2020-08-07 01:48:22 -0700

    Style <Vote>

--- src/frontend/Vote.tsx
+++ src/frontend/Vote.tsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 import { observer } from 'mobx-react';
 
+import { Button, ButtonGroup, ToggleButton } from './ui';
 import { StoreProps } from './WerewolfState';
 import useSharedSocket from './use-shared-socket';
 
 const Vote = observer(({ store }: StoreProps): JSX.Element => {
-    const [choice, setChoice] = useState('$'),
+    const [choice, setChoice] = useState(''),
         [submitted, setSubmitted] = useState(false),
         [sendMessage] = useSharedSocket();
 
-    function submitVote() {
+    function submitVote(noOne: boolean) {
         sendMessage(JSON.stringify({
             type: 'vote',
-            vote: choice,
+            vote: noOne ? '' : choice,
         }));
         setSubmitted(true);
     }
@@ -21,36 +22,28 @@ const Vote = observer(({ store }: StoreProps): JSX.Element => {
         <>
             <h1>Vote!</h1>
             <p>
-                Vote for a player to be executed. You can vote not to execute anybody, but everyone
+                Vote for a player to be executed. You may vote not to execute anybody, but everyone
                 else must do the same in order for no one to be executed.
             </p>
-            {store.playersInGame.map(p => (
-                <label htmlFor={p.id} key={p.id}>
-                    <input
-                        type="radio"
-                        id={p.id}
-                        name="choice"
+            <ButtonGroup wrap align="center">
+                {store.playersInGame.map(p => (
+                    <ToggleButton
+                        key={p.id}
                         checked={choice == p.id}
                         onChange={() => setChoice(p.id)}
-                    />
-                    {p.nick}
-                    <br />
-                </label>
-            ))}
-            <br />
-            <label htmlFor="no-one">
-                <input
-                    type="radio"
-                    id="no-one"
-                    name="choice"
-                    checked={choice == ''}
-                    onChange={() => setChoice('')}
-                />
-                No one (vote for the center)
-            </label>
-            <p>
-                <button disabled={choice == '$' || submitted} onClick={submitVote}>Vote</button>
-            </p>
+                    >
+                        {p.nick}
+                    </ToggleButton>
+                ))}
+            </ButtonGroup>
+            <ButtonGroup align="center">
+                <Button disabled={submitted} onClick={() => submitVote(true)} color="gray">
+                    Vote for no one
+                </Button>
+                <Button disabled={choice == '' || submitted} onClick={() => submitVote(false)}>
+                    Vote{choice != '' && ` for ${store.playersInGame.find(p => p.id == choice)!.nick}`}
+                </Button>
+            </ButtonGroup>
         </>
     );
 });
